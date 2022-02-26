57c5893660bf0e55d35c5f38f6e93ca332f195f0
Villain_uni <namo4862@naver.com>
2022-02-26 19:00:03 +0900

    [ADD, UPDATE] add CreateBoard / update App,index,Board

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Board from "./pages/board/Board";
+import CreateBoard from "./pages/board/CreateBoard";
 import Main from "./pages/Main/Main";
 import Visitor from "./pages/visitors/Visitor";
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/board" element={<Board />}/>
-        <Route path="/Visitor" element={<Visitor />}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/board" element={<Board />}/>
+          <Route path="/visitor" element={<Visitor />}/>
+          <Route path="/createboard" element={<CreateBoard />}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
